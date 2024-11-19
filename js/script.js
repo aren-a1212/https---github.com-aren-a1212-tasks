e7956d06d4572c0d81ed95b01705027004cd063d
aren-a1212 <abdelazizahmed1212@gmail.com>
2024-11-19 08:42:27 -0600

    updated errors

--- js/script.js
+++ js/script.js
@@ -16,9 +16,7 @@ function newItem() {
         li.toggleClass("strike");
     }
     
-    li.on("dblclick", function crossOut() {
-        li.toggleClass("strike");
-    });
+    li.on("dblclick", crossOut); 
     
     //3. Adding a delete button
     let crossOutButton = $("<crossOutButton></crossOutButton>");
